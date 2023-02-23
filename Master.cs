b87840b6ab194947f1daa5435cea712c85e397e8
Andrew-Flame <andrewflame@mail.ru>
2023-02-23 15:48:09 +0300

    Configure the project

--- Master.cs
+++ Master.cs
@@ -1,5 +1,14 @@
-﻿namespace AwesomeAchievements {
-    public class Master {
-        
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace AwesomeAchievements {
+    [BepInPlugin(ModInfo.GUID, ModInfo.TITLE, ModInfo.VERSION)]
+    public class Master : BaseUnityPlugin {
+        void Awake() {
+            #region Harmony patch
+            Harmony harmony = new Harmony(ModInfo.GUID);
+            harmony.PatchAll();
+            #endregion
+        }
     }
 }
---
+++ ModInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace AwesomeAchievements {
+    public static class ModInfo {
+        public const string TITLE = "AwesomeAchievements",
+            AUTHOR = "Flame",
+            GUID = AUTHOR + "." + TITLE,
+            VERSION = "1.0.0";
+    }
+}
