dfb3da3dc490d1a2b4aba531d3e0af3b9223223e
Andrew-Flame <andrewflame@mail.ru>
2023-03-15 21:47:49 +0300

    Add a JSON object for achievements

---
+++ AchievementsList/AchievementJSON.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+namespace AwesomeAchievements.AchievementsList; 
+
+internal struct AchievementJsonObject {
+    public string id { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+}
+
+internal struct AchievementJsonArray {
+    public AchievementJsonObject[] data { get; set; }
+}
