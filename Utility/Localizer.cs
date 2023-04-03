eb99d3069b489a1491749a45811df8181e8cc03b
Andrew-Flame <andrewflame@mail.ru>
2023-04-03 14:37:11 +0300

    Add a Localizer

---
+++ Utility/Localizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace AwesomeAchievements.Utility; 
+
+internal static class Localizer {
+    // ReSharper disable once UnusedAutoPropertyAccessor.Local
+    public static string AchievePanelHeader { get; private set; }
+    
+    public static void Init(string language) {
+        const string resourceNamespace = "AwesomeAchievements.Localization";
+        ResourceReader localeReader = new ResourceReader($"{resourceNamespace}.{language}.ini");
+        var properties = typeof(Localizer).GetProperties();
+        
+        foreach (string locale in localeReader.GetStringReader()) {
+            foreach (PropertyInfo property in properties) {
+                if (!locale.StartsWith(property.Name)) continue;
+                string[] partedLocale = locale.Trim().Split('=');
+                string localeValue = partedLocale[partedLocale.Length - 1].Trim();
+                property.SetValue(null, localeValue);
+            }
+        }
+    }
+}
--- Utility/ResourceReader.cs
+++ Utility/ResourceReader.cs
@@ -15,38 +15,32 @@ internal class ResourceReader {
     }
 
     public IEnumerable<string> GetStringReader() {
-        var resourceStream = OpenResourceStream();
-        var streamReader = OpenStreamReader(resourceStream);
+        using Stream resourceStream = _assembly.GetManifestResourceStream(_resource);
+        if (resourceStream == null) throw new UnityException("Can't read embedded resource");
+        
+        using StreamReader streamReader = new StreamReader(resourceStream);
+        if (streamReader == null) throw new UnityException("Can't create a stream reader");
+        
         while (!streamReader.EndOfStream) yield return streamReader.ReadLine();
     }
 
     public string ReadAllStrings() {
-        var resourceStream = OpenResourceStream();
-        var streamReader = OpenStreamReader(resourceStream);
+        using Stream resourceStream = _assembly.GetManifestResourceStream(_resource);
+        if (resourceStream == null) throw new UnityException("Can't read embedded resource");
+        
+        using StreamReader streamReader = new StreamReader(resourceStream);
+        if (streamReader == null) throw new UnityException("Can't create a stream reader");
+        
         return streamReader.ReadToEnd();
     }
 
     public byte[] ReadAllBytes() {
-        var resourceStream = OpenResourceStream();
-        var binaryReader = OpenBinaryReader(resourceStream);
-        return binaryReader.ReadBytes((int)resourceStream.Length);
-    }
-
-    private Stream OpenResourceStream() {
         using Stream resourceStream = _assembly.GetManifestResourceStream(_resource);
         if (resourceStream == null) throw new UnityException("Can't read embedded resource");
-        return resourceStream;
-    }
-
-    private static StreamReader OpenStreamReader(Stream resourceStream) {
-        using StreamReader streamReader = new StreamReader(resourceStream!);
-        if (streamReader == null) throw new UnityException("Can't create a stream reader");
-        return streamReader;
-    }
-
-    private static BinaryReader OpenBinaryReader(Stream resourceStream) {
+        
         using BinaryReader binaryReader = new BinaryReader(resourceStream!);
         if (binaryReader == null) throw new UnityException("Can't create a binary reader");
-        return binaryReader;
+        
+        return binaryReader.ReadBytes((int)resourceStream.Length);
     }
 }
