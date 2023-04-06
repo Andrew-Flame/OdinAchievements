5290336378384c76a10fcaf5f56011caf0c3ed49
Andrew-Flame <andrewflame@mail.ru>
2023-04-06 10:46:29 +0300

    Update announcement color

--- AchieveAnnounce/Announcer.cs
+++ AchieveAnnounce/Announcer.cs
@@ -7,8 +7,7 @@ internal static class Announcer {
     public const string PLACEHOLDER = "\rGot an achievement, but you won't see it, since you don't have the AwesomeAchievements mod installed";
     public static void Announce(string achievementName) {
         string playerName = Player.m_localPlayer.GetPlayerName();
-        string message = $"{DISTINCTIVE}{Localizer.Player} " +
-                         $"<color=orange>{playerName}</color> " +
+        string message = $"{DISTINCTIVE}{Localizer.Player} {playerName.ToUpper()}" +
                          $"{Localizer.ChatMessage} <color=green>[{achievementName.ToUpper()}]</color>{PLACEHOLDER}";
         Chat.instance.SendText(Talker.Type.Shout, message);
     }
