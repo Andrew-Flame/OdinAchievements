6b61b95781495b9229cd061effab0db7c28afa61
Andrew-Flame <andrewflame@mail.ru>
2023-04-07 21:04:50 +0300

    Improve the announcer

--- Utility/Announcer.cs
+++ Utility/Announcer.cs
@@ -4,7 +4,7 @@
 internal static class Announcer {
     /* This alert will be shown to those who haven't installed the mod. It ends with a null character, therefore nothing after it will be displayed.
      * For those who have installed the modification, this part of the line is removed and a normal message is displayed.*/
-    public const string NOT_INSTALLED_ALERT = "This player has just completed an achievement in the 'AwesomeAchievements' mod. " + 
+    public const string NOT_INSTALLED_ALERT = "This player has just completed an achievement in the '" + ModInfo.TITLE + "' mod. " + 
                                               "If you want to know more, find this mod on NexusMods or Thunderstore\0";
     
     /* Method for announce the achievement name in the game chat
