18cb5910f75c6d6a3bea8d136440c2ec4af88589
Andrew-Flame <andrewflame@mail.ru>
2023-04-10 18:49:53 +0300

    Add the method for getting a save directory

--- GameClasses/GamePlayerProfile.cs
+++ GameClasses/GamePlayerProfile.cs
@@ -1,15 +1,25 @@
-﻿using AwesomeAchievements.Utility;
+﻿using System.IO;
+using AwesomeAchievements.Utility;
 using HarmonyLib;
-using static PlayerProfile;
 
 namespace AwesomeAchievements.GameClasses; 
 
 internal static class GamePlayerProfile {
+    private static string _playerFolder;
+    
     [HarmonyPatch(typeof(PlayerProfile), "LoadPlayerData")]
     private static class PlayerProfileLoadPlayerData {
-        private static void Postfix(Player player) {
+        private static void Postfix(Player player, PlayerProfile __instance) {
             if (player != Player.m_localPlayer) return;  //If the loading player isn't a local player, exit the method
+            _playerFolder = __instance.GetPath();  //Save the relative path to the save folder
             AchievesContainer.Init();  //Initialize the achievement container
         }
+    }
+
+    /* Method for getting a directory info of the folder where characters are saved
+     return a directory info of the save folder */
+    public static DirectoryInfo GetSaveDirectory() {
+        string path = $"{Utils.persistantDataPath}/{_playerFolder}";  //Get the folder where the current player is saved
+        return new DirectoryInfo(path).Parent;  //Get the parent directory of this path
     }
 }
