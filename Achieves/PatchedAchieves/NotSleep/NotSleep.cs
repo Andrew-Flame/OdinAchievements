4e3ee5cd8d37039ef38aa6b9edb5725606de1a5e
Andrew-Flame <andrewflame@mail.ru>
2023-04-11 14:46:57 +0300

    Add the empty achievement class

---
+++ Achieves/PatchedAchieves/NotSleep/NotSleep.cs
@@ -0,0 +1,8 @@
+﻿namespace AwesomeAchievements.Achieves.PatchedAchieves.NotSleep;
+
+internal sealed class NotSleep : SingleAchievement {
+    public NotSleep(string name, string description) : base(name, description) { }
+    protected override void InitPatchers() {
+
+    }
+}
