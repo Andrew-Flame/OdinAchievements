b461858472a49a8c2aa09a6e0864e2e3ded70fe4
Andrew-Flame <andrewflame@mail.ru>
2023-04-13 15:43:22 +0300

    Add a class for getting materials from the game

--- AchieveTab/TabManager.cs
+++ AchieveTab/TabManager.cs
@@ -1,5 +1,6 @@
 ﻿using AwesomeAchievements.Utility;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AwesomeAchievements.AchieveTab; 
 
@@ -8,9 +9,23 @@ internal static class TabManager {
     
     /* Method for initializing this type */
     public static void Init() {
+ 
+        
+        
+        return;
         /* Get required game objects */
         GameObject rootInventory = InventoryGui.instance.gameObject;
         GameObject infoPanel = InventoryGui.instance.m_infoPanel.gameObject;
+
+        /* Init the achievement tab icon */
+        GameObject achieveIcon = new GameObject("Achievement_Icon", typeof(Image));
+        achieveIcon.transform.SetParent(infoPanel.transform);
+        achieveIcon.transform.localPosition = new Vector3(0f, 0f);
+
+        ResourceReader iconReader = new ResourceReader("Assets.Textures.AchievementTabIcon.png");
+        Image iconImage = achieveIcon.GetComponent<Image>();
+        iconImage.sprite = iconReader.GetSprite();
+        iconImage.mainTexture.wrapMode = TextureWrapMode.Clamp;
 
         LogInfo.Log("An achievement tab manager has been initialized");
     }
---
+++ Utility/Materials.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AwesomeAchievements.Utility; 
+
+/* Class for get the materials from the game because it's the easiest way to get them */
+internal static class Materials {
+    public static Material Litpanel =>
+        GameObject.Find("_GameMain/LoadingGUI/PixelFix/IngameGui(Clone)/Inventory_screen/root/Info/bkg/").GetComponent<Image>().material;
+}
