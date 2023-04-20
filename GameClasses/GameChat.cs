c27a6b9adf58e2c664e5f46d86d185b57b28c13e
Andrew-Flame <andrewflame@mail.ru>
2023-04-20 12:56:59 +0300

    Improve the announcer work

--- GameClasses/GameChat.cs
+++ GameClasses/GameChat.cs
@@ -1,5 +1,6 @@
 ﻿using VikingAchievements.Utility;
 using HarmonyLib;
+using UnityEngine;
 using static Chat;
 
 namespace VikingAchievements.GameClasses; 
@@ -9,17 +10,21 @@ internal static class GameChat {
     [HarmonyPatch(typeof(Chat), "OnNewChatMessage")]
     private class ChatOnNewChatMessage {
         private const byte GAP_SIZE = 4, 
-                           MESSAGE_SIZE = 20;
-        
-        private static bool Prefix(string text, string user, ref float ___m_hideTimer) {
+                           FONT_SIZE = 20;
+
+        /* Patch-method for redefine the behavior of the game chat
+         * go - the game object which sent a message
+         * text - the text of the message
+         * ___m_hideTimer - timer for delaying the hiding of the chat */
+        private static bool Prefix(GameObject go, string text, ref float ___m_hideTimer) {
             /* If it's not a "completing an achievement" message, exec the original method */
             if (!text.StartsWith(Announcer.NOT_INSTALLED_ALERT)) return true;
             
             /* If it's a required message, run the code below */
-            if (user == Player.m_localPlayer.GetPlayerName()) ___m_hideTimer = -3f;  //Get an extra showing chat time, if there isn't our achievement
+            if (go != Player.m_localPlayer.gameObject) ___m_hideTimer = -3f;  //Get an extra showing chat time, if there isn't our achievement
             string message = text.Replace(Announcer.NOT_INSTALLED_ALERT, "");  //Remove the "not mod installed alert" from the string
             instance.AddString($"<size={GAP_SIZE.ToString()}>\n</size>" + 
-                               $"<b><size={MESSAGE_SIZE.ToString()}>{message}</size></b>");  //Print the message into the chat
+                               $"<b><size={FONT_SIZE.ToString()}>{message}</size></b>");  //Print the message into the chat
             return false;  //Don't exec the original method
         }
     }
