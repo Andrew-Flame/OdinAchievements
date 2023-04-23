7566233610409a175f5cfcb14822c08b8bc120c5
Andrew-Flame <andrewflame@mail.ru>
2023-04-23 19:53:00 +0300

    Improve the achievement icon

--- UI/Icon/IconManager.cs
+++ UI/Icon/IconManager.cs
@@ -6,52 +6,60 @@ namespace VikingAchievements.UI.Icon;
 
 /* Class for work with achievement tab */  
 internal static class IconManager {
+    private static Image _iconImage;
+    
     /* Method for initializing this type */
     public static void Init() {
         /* Get required game objects */
         GameObject infoPanel = InventoryGui.instance.m_infoPanel.gameObject;
 
         /* Init the achievement tab icon */
-        GameObject achieveIcon = new GameObject("Achievements", typeof(Button));
-        achieveIcon.transform.SetParent(infoPanel.transform);
-        achieveIcon.transform.localPosition = new Vector3(0f, 0f);
+        GameObject iconParent = new GameObject("Achievements", typeof(Button));
+        iconParent.transform.SetParent(infoPanel.transform);
+        iconParent.transform.localPosition = new Vector3(0f, 0f);
 
-        SetDarken(achieveIcon.transform);
-        SetTexture(achieveIcon.transform);
-        SetSize(achieveIcon.transform);
-        SetPosition(achieveIcon.transform);
-        SetButtonComponent(achieveIcon.transform);
+        SetDarken(iconParent.transform);
+        SetTexture(iconParent.transform);
+        SetSize(iconParent.transform);
+        SetPosition(iconParent.transform);
+        SetButtonComponent(iconParent.transform);
 
         LogInfo.Log("An achievement tab manager has been initialized");
     }
 
-    private static void SetDarken(Transform achieveIcon) {
+    private static void SetDarken(Transform iconParent) {
         /* Copy the background object */
         Transform infoPanel = InventoryGui.instance.m_infoPanel;
-        GameObject darken = Object.Instantiate(infoPanel.Find("Texts/Background").gameObject, achieveIcon, true);
+        GameObject darken = Object.Instantiate(infoPanel.Find("Texts/Background").gameObject, iconParent, true);
         darken.name = "Background";
         darken.transform.localPosition = new Vector3(0f, 0f);
     }
     
-    private static void SetTexture(Transform achieveIcon) {
+    private static void SetTexture(Transform iconParent) {
         /* Create an image object */
-        GameObject icon = new GameObject("Icon", typeof(Image));
-        icon.transform.SetParent(achieveIcon);
-        icon.transform.localPosition = new Vector3(0f, 0f);
-        
+        Transform icon = new GameObject("Icon", typeof(Image)).transform;
+        icon.SetParent(iconParent);
+        icon.localPosition = new Vector3(0f, 0f);
+
         /* Set the texture */
-        ResourceReader iconReader = new ResourceReader("Assets.Textures.IconDefault.png");
-        Image image = icon.GetComponent<Image>();
-        image.sprite = iconReader.GetSprite(FilterMode.Point);
-        image.mainTexture.wrapMode = TextureWrapMode.Clamp;
+        ResourceReader iconReader = new ResourceReader("Assets.Textures.Icon.png");
+        _iconImage = icon.GetComponent<Image>();
+        _iconImage.sprite = iconReader.GetSprite(FilterMode.Point);
+        _iconImage.mainTexture.wrapMode = TextureWrapMode.Clamp;
+        _iconImage.color = new Color(1f, 0.7176f, 0.3603f);
+        
+        /* Set the material */
+        Material material = InventoryGui.instance.m_infoPanel.Find("Texts/Icon").GetComponent<Image>().material;
+        _iconImage.material = material;
     }
 
-    private static void SetSize(Transform achieveIcon) {
-        Vector2 size = new Vector2(60f, 60f);
-        achieveIcon.Find("Icon").GetComponent<RectTransform>().sizeDelta = size;
+    private static void SetSize(Transform iconParent) {
+        const float sideSize = 58f;
+        Vector2 size = new Vector2(sideSize, sideSize);
+        iconParent.Find("Icon").GetComponent<RectTransform>().sizeDelta = size;
     }
 
-    private static void SetPosition(Transform achieveIcon) {
+    private static void SetPosition(Transform iconParent) {
         /* Get game objects */
         Transform infoPanel = InventoryGui.instance.m_infoPanel;
         Transform texts = infoPanel.Find("Texts");
@@ -69,14 +77,15 @@ internal static class IconManager {
         skills.localPosition = new Vector3(-offset * 4f, height);
         trophies.localPosition = new Vector3(-offset * 3f, height);
         pvp.localPosition = new Vector3(-offset * 2f, height);
-        achieveIcon.localPosition = new Vector3(-offset, height);
+        iconParent.localPosition = new Vector3(-offset, height);
     }
 
-    private static void SetButtonComponent(Transform achieveIcon) {
-        Button button = achieveIcon.GetComponent<Button>();
-        Image image = achieveIcon.GetComponent<Image>();
-        
-        button.image = image;
-        button.targetGraphic = image;
+    private static void SetButtonComponent(Transform iconParent) {
+        Button button = iconParent.GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private static void OnClick() {
+        _iconImage.color = new Color(1f, 1f, 1f, 0.2f);
     }
 }
